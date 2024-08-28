fa7d6c29807bfecc0feab3cb58249476436a22fa
satria bahari <satriaabaharii@gmail.com>
2024-08-28 11:30:15 +0700

    style: add some fonts

--- src/common/styles/fonts.ts
+++ src/common/styles/fonts.ts
@@ -1,4 +1,4 @@
-import { Sora, Onest } from "next/font/google";
+import { Sora, Onest, Plus_Jakarta_Sans } from "next/font/google";
 
 export const soraSans = Sora({
   variable: "--soraSans-font",
@@ -9,6 +9,13 @@ export const soraSans = Sora({
 
 export const onestSans = Onest({
   variable: "--onestSans-font",
+  subsets: ["latin"],
+  display: "fallback",
+  weight: ["300", "400", "500", "600", "700", "800"],
+});
+
+export const plusJakartaSans = Plus_Jakarta_Sans({
+  variable: "--plusJakartaSans-font",
   subsets: ["latin"],
   display: "fallback",
   weight: ["300", "400", "500", "600", "700", "800"],
