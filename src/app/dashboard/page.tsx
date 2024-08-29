b8078e8518edeaa1bb017add1516300c411355fc
satria bahari <satriaabaharii@gmail.com>
2024-08-29 16:54:30 +0700

    feat: add dashboard feature

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import Container from '@/common/components/elements/Container'
+import PageHeading from '@/common/components/elements/PageHeading'
+import Dashboard from '@/modules/dashboard'
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <>
+      {/* <PageHeading
+        title="My Blogs: Stories and Creators"
+        description="Lorem ipsum dolor sit amet consectetur. Elementum amet congue purus scelerisque."
+      /> */}
+      <Dashboard/>
+    </>
+  )
+}
+
+export default DashboardPage
