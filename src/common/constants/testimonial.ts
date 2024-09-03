fe35f487f1c85ba418d65532135d6ad01ce7eec9
satria bahari <satriaabaharii@gmail.com>
2024-09-03 21:08:29 +0700

    refactor: add testimonial constant

--- src/common/constants/testimonial.ts
+++ src/common/constants/testimonial.ts
@@ -25,6 +25,30 @@ const TESTIMONIAL_ITEMS: TestimonialProps[] = [
       "A fantastic experience from start to finish. Customer service is outstanding.",
     isShow: true,
   },
+  {
+    image: "/images/hero.jpg",
+    name: "Sarah Connor",
+    position: "CEO of FutureTech",
+    message:
+      "This product has completely transformed our business. Highly recommend to everyone!",
+    isShow: true,
+  },
+  {
+    image: "/images/hero.jpg",
+    name: "James Cameron",
+    position: "Creative ",
+    message:
+      "The attention to detail and the quality of service provided is top-notch.",
+    isShow: true,
+  },
+  {
+    image: "/images/hero.jpg",
+    name: "Linda Hamilton",
+    position: "Head of Marketing",
+    message:
+      "A fantastic experience from start to finish. Customer service is outstanding.",
+    isShow: true,
+  },
 ];
 
 export default TESTIMONIAL_ITEMS;
