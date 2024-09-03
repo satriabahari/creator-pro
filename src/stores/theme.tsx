44353ee1582aa1b39262ddf36dd275054cc3452c
satria bahari <satriaabaharii@gmail.com>
2024-09-03 21:08:47 +0700

    refactor: add theme store

--- src/stores/theme.tsx
+++ src/stores/theme.tsx
@@ -1,15 +1,9 @@
 "use client";
 
-import { ThemeProvider } from "next-themes";
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
 
-export default function ThemeProviderContext({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <ThemeProvider attribute="class" defaultTheme="light">
-      {children}
-    </ThemeProvider>
-  );
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
