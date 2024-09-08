7cb154d8c575f718a4cdef5309d39498355edd54
satria bahari <satriaabaharii@gmail.com>
2024-09-08 23:01:36 +0700

    refactor: add some property

--- src/common/constants/features.ts
+++ src/common/constants/features.ts
@@ -2,37 +2,48 @@ import { FeatureProps } from "../types/feature";
 
 const FEATURE_ITEMS: FeatureProps[] = [
   {
+    id: 1,
     title: "Seamless Theme Switching: Light to Dark",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius aperiam labore fugit tempore ab qui molestias voluptatum possimus saepe doloribus",
     image: "/images/hero.jpg",
-    aosType: "fade-left",
+    aosTitle: "zoom-in",
+    aosImage: "fade-up-left",
     isShow: true,
   },
   {
+    id: 2,
     title: "Dashboard: Light to Dark",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius aperiam labore fugit tempore ab qui molestias voluptatum possimus saepe doloribus",
     image: "/images/hero.jpg",
     positionGrid: "order-first",
-    aosType: "fade-right",
+    // aosTitle: "fade-left",
+    aosTitle: "zoom-in",
+    aosImage: "fade-up-right",
     isShow: true,
   },
   {
+    id: 3,
     title: "Task: Light to Dark",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius aperiam labore fugit tempore ab qui molestias voluptatum possimus saepe doloribus",
     image: "/images/hero.jpg",
-    aosType: "fade-left",
+    // aosTitle: "fade-right",
+    aosTitle: "zoom-in",
+    aosImage: "fade-up-left",
     isShow: true,
   },
   {
+    id: 4,
     title: "Monetization: Light to Dark",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius aperiam labore fugit tempore ab qui molestias voluptatum possimus saepe doloribus",
     image: "/images/hero.jpg",
     positionGrid: "order-first",
-    aosType: "fade-right",
+    // aosTitle: "fade-left",
+    aosTitle: "zoom-in",
+    aosImage: "fade-up-right",
     isShow: true,
   },
 ];
