bb35dd7f9a2ec09a77eaa7030f43f475a126664d
satria bahari <satriaabaharii@gmail.com>
2024-09-08 23:07:57 +0700

    refactor: deactivate some sections

--- src/modules/dashboard/components/Dashboard.tsx
+++ src/modules/dashboard/components/Dashboard.tsx
@@ -1,22 +1,24 @@
 import React from "react";
 import InfoList from "./InfoList";
 import TaskListCard from "./TaskListCard";
-import FollowerListCard from "./FollowerListCard";
+// import FollowerListCard from "./FollowerListCard";
 import VisitListCard from "./VisitListCard";
-import SocialMediaStats from "./SocialMediaStats";
+// import SocialMediaStats from "./SocialMediaStats";
 import Integration from "./Integration";
 
 const Dashboard = () => {
   return (
-    <div className=" space-y-4">
+    <div className="space-y-4">
       <InfoList />
       <div className="grid grid-cols-3 gap-4">
         <TaskListCard />
-        <FollowerListCard />
+        {/* <FollowerListCard /> */}
+        <div></div>
         <VisitListCard />
       </div>
       <div className="grid grid-cols-3 gap-4">
-        <SocialMediaStats />
+        {/* <SocialMediaStats /> */}
+        <div></div>
         <Integration />
       </div>
     </div>
