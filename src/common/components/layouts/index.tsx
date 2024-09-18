4dff64e613e80363e898540b7e47ff4e49d9b447
satria bahari <satriaabaharii@gmail.com>
2024-09-18 15:50:06 +0700

    refactor: change some code

--- src/common/components/layouts/index.tsx
+++ src/common/components/layouts/index.tsx
@@ -33,7 +33,7 @@ const Layouts = ({ children }: LayoutsProps) => {
           <div className="absolute bottom-0 left-0 right-0 top-0 z-[-1] bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:64px_64px] [mask-image:radial-gradient(ellipse_60%_100%_at_50%_0%,#000_70%,transparent_100%)]" />
           <div className="pb-4">
             <Header />
-            <main className="px-16">{children}</main>
+            <main className="lg:px-16 px-8">{children}</main>
             <Footer />
           </div>
         </>
