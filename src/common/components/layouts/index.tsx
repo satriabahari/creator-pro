4f57a98871f39b838e32aaf2377255b1cb63ce39
satria bahari <satriaabaharii@gmail.com>
2024-09-28 18:29:44 +0700

    refactor: disable header

--- src/common/components/layouts/index.tsx
+++ src/common/components/layouts/index.tsx
@@ -6,7 +6,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 // import Footer from "./footer/Footer";
-import Header from "./header/Header";
+// import Header from "./header/Header";
 
 interface LayoutsProps {
   children: React.ReactNode;
@@ -31,7 +31,7 @@ const Layouts = ({ children }: LayoutsProps) => {
         <>
           <div className="absolute bottom-0 left-0 right-0 top-0 z-[-1] bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:64px_64px] [mask-image:radial-gradient(ellipse_60%_100%_at_50%_0%,#000_70%,transparent_100%)]" />
           <div className="pb-4">
-            <Header />
+            {/* <Header /> */}
             <main className="px-8 lg:px-16">{children}</main>
             {/* <Footer /> */}
           </div>
